fe0fbbb150e1326d9282ef94947fe710740e6a3a
Tavishi-Maini <153937358+Tavishi-Maini@users.noreply.github.com>
2024-01-11 23:19:41 +0530

    Add files via upload

---
+++ courseselect.js
@@ -0,0 +1,10 @@
+<Selectivity.React
+    allowClear={true}
+    items={['ESC111', 'ESC112','MTH111','MTH112','MTH113','MTH114', 'PE111', 'PE112', 'TA111', 'ETH111', 'LIF111', 'CHM112', 'CHM113']}
+    placeholder='No city selected'
+/>
+return(
+    <div>
+        <button onClick={logIn}>Submit</button>
+    </div>
+)
